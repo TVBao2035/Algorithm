104f64828e28a65a488c7294d9e968a1e669fa51
TVBao2003 <truongvanbao111333@gmail.com>
2023-06-10 01:00:44 +0700

    add 27,28,29,30,31,32,33

--- section_27/section_27/section_27.cpp
+++ section_27/section_27/section_27.cpp
@@ -1,20 +1,48 @@
-﻿// section_27.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include<iostream>
+#include<vector>
+#include<stack>
 
-#include <iostream>
+using namespace std;
+/*
+Cho một danh sách các số nguyên dương arr dưới dạng vector<int>. 
+Bạn cần phải đưa từng phần tử trong danh sách này vào stack. 
+Sau đó output giá trị nhỏ nhất trong stack trước mỗi lần pop(). 
+Xem ví dụ để hiểu rõ hơn.
+
+Ví dụ
+
+Với arr = [4,2,6,7,8,1] thìpopMin(arr) = [1,2,2,2,2,4]
+Giải thích: từ arr có được stack: [4,2,6,7,8,1] với 1 là top. 
+Trước khi pop() thì 1 là giá trị nhỏ nhất và sau khi pop() stack trở thành [4,2,6,7,8] 
+và 2 là giá trị nhỏ nhất, cứ thế ta có kết quả [1,2,2,2,2,4].
+*/
+vector<int> minValues(vector<int> arrayNumbers) {
+	stack<int> storeMinValues;
+	storeMinValues.push(arrayNumbers.front());
+	for (int number : arrayNumbers) {
+		if (storeMinValues.top() < number) storeMinValues.push(storeMinValues.top());
+		else storeMinValues.push(number);
+		
+	}
+	vector<int> result;
+	while (!storeMinValues.empty()) {
+		result.push_back(storeMinValues.top());
+		storeMinValues.pop();
+	}
+	return result;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<int> arrayNumbers;
+	int lengthOfArray;
+	cout << "enter length of array: "; cin >> lengthOfArray;
+	cout << "enter elements in Array:" << endl;
+	for (int i = 0; i < lengthOfArray; i++) {
+		int store; cin >> store; arrayNumbers.push_back(store);
+	}
+	cout << "display result: ";
+	arrayNumbers = minValues(arrayNumbers);
+	for (int number : arrayNumbers) cout << number << " ";
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
